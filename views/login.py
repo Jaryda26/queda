d06674f7470cae3db5e1d012cfe9d9789270bc7f
Jorge <jyanez_99@yahoo.com>
2026-09-19 09:18:56 -0600

    Categorias

--- views/login.py
+++ views/login.py
@@ -1,12 +1,47 @@
 import streamlit as st
+
 from services.usuario_service import validar_usuario
 from auth.session import login_user
 
+
 def pantalla_login():
- st.title("Login")
- e=st.text_input("Correo")
- p=st.text_input("Contraseña",type="password")
- if st.button("Entrar"):
-  u=validar_usuario(e,p)
-  if u is not None: login_user(int(u['id']),u['nombre'],u['email']); st.rerun()
-  else: st.error("Credenciales inválidas")
+
+    st.title("🔐 Login")
+
+    with st.form("form_login"):
+
+        email = st.text_input(
+            "Correo"
+        )
+
+        password = st.text_input(
+            "Contraseña",
+            type="password"
+        )
+
+        entrar = st.form_submit_button(
+            "Entrar"
+        )
+
+    if entrar:
+
+        usuario = validar_usuario(
+            email,
+            password
+        )
+
+        if usuario is not None:
+
+            login_user(
+                int(usuario["id"]),
+                usuario["nombre"],
+                usuario["email"]
+            )
+
+            st.rerun()
+
+        else:
+
+            st.error(
+                "Credenciales inválidas"
+            )
